12f49ac58d5237fbf3951e12ae160f3051d6f0eb
Besim Mustafi <besimnaim@gmail.com>
2023-11-10 12:21:13 +0100

    Udemy_Video 35_ArrowFunction

--- script.js
+++ script.js
@@ -6,11 +6,10 @@ const testPass = true;
 
 if (testPass) hasDriversLicense = true;
 if (hasDriversLicense) console.log(`It is allowed to drive for you`);
-*/
+
 
 // Function and expressions
-//Function declaration: The function can be called befor or after the calculation
-const currentAge = ageBesim(1991); //Function called
+const currentAge = ageBesim(1991); //Function declaration: The function can be called befor or after the calculation
 function ageBesim(calcBirthYear){
 return 2023 - calcBirthYear;
 }
@@ -25,3 +24,19 @@ return naileAge;
 }
 const currentAgeNaile = ageNaile(1994);
 console.log(currentAgeNaile);
+*/
+
+//Arrow Function: Special form of an expression function
+const calcAge3 = birthyear => 2023 - birthyear; //Just one line of code
+const age3 = calcAge3(1991);
+console.log(age3);
+
+
+const yearsUntilRetirement = (birtYear, firstName) => {  //More then one line of code
+    let age = 2023 - birtYear;
+    let rertirement = 67 - age;
+    //return rertirement;
+    return `${firstName} geht in ${rertirement} Jahren in den Ruhestand`;
+} 
+console.log(yearsUntilRetirement(1991, "Besim"));
+
