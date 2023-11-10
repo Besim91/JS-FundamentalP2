e54b5a4551d32d5e6775f5743801de18c266551e
Besim Mustafi <besimnaim@gmail.com>
2023-11-10 10:39:50 +0100

    Udemy_Video 34_Functions

--- script.js
+++ script.js
@@ -1,7 +1,27 @@
+//Udemy Video 32
 'use strict'; //It shows us the exact line where the buck is hidden. Without this function we will get just an error alert with a general message
-
+/*
 let hasDriversLicense = false;
 const testPass = true;
 
 if (testPass) hasDriversLicense = true;
 if (hasDriversLicense) console.log(`It is allowed to drive for you`);
+*/
+
+// Function and expressions
+//Function declaration: The function can be called befor or after the calculation
+const currentAge = ageBesim(1991); //Function called
+function ageBesim(calcBirthYear){
+return 2023 - calcBirthYear;
+}
+console.log(currentAge);
+
+
+//Function expression
+//Function can be called just after the calculation
+const ageNaile = function(calcBirthYear2){
+const naileAge = 2023 - calcBirthYear2;
+return naileAge;
+}
+const currentAgeNaile = ageNaile(1994);
+console.log(currentAgeNaile);
